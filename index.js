0d94d7d916da003d6c4ebd5a4c13ab42c8bed476
Hamed Moodi <hamedmoodi2011@gmail.com>
2023-12-21 12:45:49 +0330

    [ADD] Add test for WP Creator

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const path          = require('path');
-
-const current_path  = process.cwd();
-
-const File_Generator = require( path.join( current_path, 'inc', 'File_Generator.js' ) );
-
-const file_generator = new File_Generator();
-
-file_generator
-    .line('<?php')
-    .line('$name = \'Hamed Moodi\';')
-    .line('echo $name;')
-    .generate()
-    .set_file_name('xyz.php')
-    .save_to( current_path + '/xyz/' );
+const path              = require('path');
+const current_path      = process.cwd() + '/';
 
